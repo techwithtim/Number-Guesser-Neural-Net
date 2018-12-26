568470de034cf810591723bbd10752fae0d84d18
Tech With Tim <35348871+techwithtim@users.noreply.github.com>
2018-12-26 11:55:01 -0500

    Add files via upload

---
+++ drawNumber.py
@@ -0,0 +1,173 @@
+import pygame
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from tkinter import *
+from tkinter import messagebox
+
+
+class pixel(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = (255,255,255)
+        self.neighbors = []
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.x + self.width, self.y + self.height))
+
+    def getNeighbors(self, g):
+        # Get the neighbours of each pixel in the grid, this is used for drawing thicker lines
+        j = self.x // 20 # the var i is responsible for denoting the current col value in the grid
+        i = self.y // 20 # the var j is responsible for denoting thr current row value in the grid
+        rows = 28
+        cols = 28
+
+        # Horizontal and vertical neighbors
+        if i < cols - 1:  # Right
+            self.neighbors.append(g.pixels[i + 1][j])
+        if i > 0:  # Left
+            self.neighbors.append(g.pixels[i - 1][j])
+        if j < rows - 1:  # Up
+            self.neighbors.append(g.pixels[i][j + 1])
+        if j > 0:  # Down
+            self.neighbors.append(g.pixels[i][j - 1])
+
+        # Diagonal neighbors
+        if j > 0 and i > 0:  # Top Left
+            self.neighbors.append(g.pixels[i - 1][j - 1])
+
+        if j + 1 < rows and i > -1 and i - 1 > 0:  # Bottom Left
+            self.neighbors.append(g.pixels[i - 1][j + 1])
+
+        if j - 1 < rows and i < cols - 1 and j - 1 > 0:  # Top Right
+            self.neighbors.append(g.pixels[i + 1][j - 1])
+
+        if j < rows - 1 and i < cols - 1:  # Bottom Right
+            self.neighbors.append(g.pixels[i + 1][j + 1])
+
+
+class grid(object):
+    pixels = []
+
+    def __init__(self, row, col, width, height):
+        self.rows = row
+        self.cols = col
+        self.len = row * col
+        self.width = width
+        self.height = height
+        self.generatePixels()
+        pass
+
+    def draw(self, surface):
+        for row in self.pixels:
+            for col in row:
+                col.draw(surface)
+
+    def generatePixels(self):
+        x_gap = self.width // self.cols
+        y_gap = self.height // self.rows
+        self.pixels = []
+        for r in range(self.rows):
+            self.pixels.append([])
+            for c in range(self.cols):
+                self.pixels[r].append(pixel(x_gap * c, y_gap * r, x_gap, y_gap))
+
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.pixels[r][c].getNeighbors(self)
+
+    def clicked(self, pos): #Return the position in the grid that user clicked on
+        try:
+            t = pos[0]
+            w = pos[1]
+            g1 = int(t) // self.pixels[0][0].width
+            g1 = int(t) // self.pixels[0][0].width
+            g2 = int(w) // self.pixels[0][0].height
+
+            return self.pixels[g2][g1]
+        except:
+            pass
+
+    def convert_binary(self):
+        li = self.pixels
+
+        newMatrix = [[] for x in range(len(li))]
+
+        for i in range(len(li)):
+            for j in range(len(li[i])):
+                if li[i][j].color == (255,255,255):
+                    newMatrix[i].append(0)
+                else:
+                    newMatrix[i].append(1)
+
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        x_test = tf.keras.utils.normalize(x_test, axis=1)
+        for row in range(28):
+            for x in range(28):
+                x_test[0][row][x] = newMatrix[row][x]
+
+        return x_test[:1]
+
+
+def guess(li):
+    model = tf.keras.models.load_model('m.model')
+
+    predictions = model.predict(li)
+    print(predictions[0])
+    t = (np.argmax(predictions[0]))
+    print("I predict this number is a:", t)
+    window = Tk()
+    window.withdraw()
+    messagebox.showinfo("Prediction", "I predict this number is a: " + str(t))
+    #plt.imshow(li[0], cmap=plt.cm.binary)
+    #plt.show()
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                li = g.convert_binary()
+                guess(li)
+                g.generatePixels()
+            if pygame.mouse.get_pressed()[0]:
+
+                pos = pygame.mouse.get_pos()
+                clicked = g.clicked(pos)
+                clicked.color = (0,0,0)
+                for n in clicked.neighbors:
+                    n.color = (0,0,0)
+
+            if pygame.mouse.get_pressed()[2]:
+                try:
+                    pos = pygame.mouse.get_pos()
+                    clicked = g.clicked(pos)
+                    clicked.color = (255,255,255)
+                except:
+                    pass
+
+        g.draw(win)
+        pygame.display.update()
+
+pygame.init()
+width = height = 560
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Number Guesser")
+g = grid(28, 28, width, height)
+main()
+
+
+pygame.quit()
+quit()
+
+
+
+
---
+++ testNetwork.py
@@ -0,0 +1,38 @@
+import keras
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# FOR TESTING
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+for test in range(len(x_test)):
+    for row in range(28):
+        for x in range(28):
+            if x_test[test][row][x] != 0:
+                x_test[test][row][x] = 1
+
+
+model = tf.keras.models.load_model('m.model')
+print(len(x_test))
+predictions = model.predict(x_test[:10])
+
+count = 0
+for x in range(len(predictions)):
+    guess = (np.argmax(predictions[x]))
+    actual = y_test[x]
+    print("I predict this number is a:", guess)
+    print("Number Actually Is a:", actual)
+    if guess != actual:
+        #print("--------------")
+        #print('WRONG')
+        #print('---------------')
+        count+=1
+    plt.imshow(x_test[x], cmap=plt.cm.binary)
+    plt.show()
+
+print("The program got", count, 'wrong, out of', len(x_test))
+print(str(100 - ((count/len(x_test))*100)) + '% correct')
+
